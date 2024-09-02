d83918480e115448e6c6d652f12c24cf042db2fa
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-09-02 16:55:11 -0300

    refactor(database): change database from sqlite to mysql

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,19 @@
 import { createConnection, Connection } from 'typeorm';
 import { Measurement } from '../models/Measurement';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const connectDatabase = async (): Promise<Connection> => {
     return createConnection({
-        type: 'sqlite',
-        database: 'shopper.sqlite',
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT!),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         entities: [Measurement],
         synchronize: true,
-        logging: true
+        logging: true,
     });
 };
