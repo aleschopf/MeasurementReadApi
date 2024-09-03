8227ef617edf0354605800a6fe52b7c5569f41f5
Alecsandro Schopf Auer Junior <alexschopf05@gmail.com>
2024-09-03 19:46:11 -0300

    fix: fix some bugs at docker run

--- node.dockerfile
+++ node.dockerfile
@@ -2,8 +2,6 @@ FROM node:22.6.0
 
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y default-mysql-client
-
 COPY package*.json ./
 
 RUN npm install
@@ -11,9 +9,6 @@ COPY . .
 RUN mkdir -p /app/dist/uploads/
 RUN npm run build
 
-COPY wait-for-mysql.sh /usr/src/app/wait-for-mysql.sh
-RUN chmod +x /usr/src/app/wait-for-mysql.sh
-
 EXPOSE 80
 
-CMD ["./wait-for-mysql.sh", "npm", "start"]
+CMD ["npm", "start"]
--- src/config/database.ts
+++ src/config/database.ts
@@ -5,15 +5,27 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const connectDatabase = async (): Promise<Connection> => {
-    return createConnection({
-        type: 'mysql',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT!),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [Measurement],
-        synchronize: true,
-        logging: true,
-    });
+    let connection: Connection | null = null;
+
+    while (!connection) {
+        try {
+            connection = await createConnection({
+                type: 'mysql',
+                host: process.env.DB_HOST,
+                port: parseInt(process.env.DB_PORT!),
+                username: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME,
+                entities: [Measurement],
+                synchronize: true,
+                logging: true,
+            });
+            console.log('Conexão com o banco de dados estabelecida com sucesso.');
+        } catch (error) {
+            console.error('Erro ao conectar ao banco de dados. Tentando novamente em 3 segundos...', error);
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+        }
+    }
+
+    return connection;
 };
