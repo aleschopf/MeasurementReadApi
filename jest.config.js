067bf4e7d80115953e14121814a4fb8edb6d38a5
CaioHVectorA <caihebatista@gmail.com>
2024-09-05 16:16:11 -0300

    Chore: add jest funcional setup

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'],
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    transform: {
-      '^.+\\.ts?$': 'ts-jest',
-    },
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'fetch': fetch,
+  },
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  setupFiles: ['<rootDir>/jest.setup.js'],
+};
