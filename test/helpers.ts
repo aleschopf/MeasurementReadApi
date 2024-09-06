554e00ae4a254b1efc6fbf6173e7e2de7e85b811
CaioHVectorA <caihebatista@gmail.com>
2024-09-05 22:33:11 -0300

    Feat: added list api route tests!

--- test/helpers.ts
+++ test/helpers.ts
@@ -4,9 +4,9 @@ export const getBase64Image = (filePath: string) => {
     return `${file.toString('base64')}`;
 };
 
-export function generateRandomMeasurement(): [string, string, string, number, string] {
+export function generateRandomMeasurement(customer_code?: string): [string, string, string, number, string] {
     return [
-        (Math.random() * 100000).toString(),
+        customer_code ?? (Math.random() * 100000).toString(),
         new Date().toISOString(),
         'WATER',
         0,
